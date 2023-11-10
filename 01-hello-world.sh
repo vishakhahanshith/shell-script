d0d0fd18d0a051b21c4a97bddea17c1d7f2149d9
vishakhahanshith <vishakhahanshith@gmail.com>
2023-11-10 19:18:24 +0530

    Started implementing variables concept

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 echo "Hello World, I am learning DevOps with AWS"
 
-echo "Ramesh: Hi Suresh, Good Morning"
-echo "Suresh: Good Morning Ramesh"
-echo "Ramesh: How are you Suresh?"
-echo "Suresh: I am good, How are you Ramesh?"
+#Implementing variables in the below script
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+#echo "Ramesh: Hi Suresh, Good Morning"
+#echo "Suresh: Good Morning Ramesh"
+#echo "Ramesh: How are you Suresh?"
+#echo "Suresh: I am good, How are you Ramesh?"
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good, How are you $PERSON1?"
