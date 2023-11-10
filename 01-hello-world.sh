bea04359f18acb74d605e221125183123a006c9b
vishakhahanshith <vishakhahanshith@gmail.com>
2023-11-10 17:34:49 +0530

    variables intro started

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Hello World, I am learning DevOps with AWS"
+
+echo "Ramesh: Hi Suresh, Good Morning"
+echo "Suresh: Good Morning Ramesh"
+echo "Ramesh: How are you Suresh?"
+echo "Suresh: I am good, How are you Ramesh?"
