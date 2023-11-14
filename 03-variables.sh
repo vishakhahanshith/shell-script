bfefa13e90151749f43027d74a1ae7878062afa4
vishakhahanshith <vishakhahanshith@gmail.com>
2023-11-14 13:38:26 +0530

    implementing 3rd type of variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Hello World, I am learning DevOps with AWS"
+
+PERSON1=$1 #This is the 1st value from command line
+PERSON2=$2 #This is the 2nd value from command line
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good, How are you $PERSON1?"
