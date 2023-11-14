e174b2c3cc5188a0f546477538e98250d0849631
vishakhahanshith <vishakhahanshith@gmail.com>
2023-11-14 12:36:38 +0530

    Implementing date command

---
+++ 02-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Shell script will run the command inside $() and stores the output into the variable called DATE
+DATE=$(date)
+echo "Hello, this script is executed at timestamp: $DATE"
