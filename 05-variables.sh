40f111ee1cf64e2d9950d996033654332e6bba23
vishakhahanshith <vishakhahanshith@gmail.com>
2023-11-14 16:12:44 +0530

    modifications in read command

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Please enter your username"
-read USERNAME # This is the variable where the value of the one user needs to enter
+read -s USERNAME # This is the variable where the value of the one user needs to enter
 echo "Username entered is: $USERNAME"
 echo "Please enter your password"
-read PASSWORD
+read -s PASSWORD
 echo "Please enter your password"
 echo "Password entered is: $PASSWORD"
