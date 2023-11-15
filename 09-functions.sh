235b39bbc78f0e9924298f99c66886b973603c95
vishakhahanshith <vishakhahanshith@gmail.com>
2023-11-15 23:24:29 +0530

    Implementing special vars

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # Our program goal is to install mysql
+DATE=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp/
 USERID=$(id -u)
 # This function should validate the previous command and should inform the user whether it is success or failure
 VALIDATE(){
     #$1 -- It will receive the argument1
     if [ $1 -ne 0 ]
 then
-   echo "Installation ... FAILURE"
+   #echo "Installation ... FAILURE"
+   echo "$2 ... FAILURE"
    exit 1
 else
-   echo "Installation ... SUCCESS"
+   #echo "Installation ... SUCCESS"
+   echo "$2 ... SUCCESS"
 fi
 }
 
---
+++ 10-special-vars.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+VAR1=$1
+VAR2=$2
+echo "Variable 1: $VAR1"
+echo "All vars: $@"
